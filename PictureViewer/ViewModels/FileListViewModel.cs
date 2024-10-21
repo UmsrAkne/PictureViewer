99398fbeb194df2307746e626bd4a4078b91b7c7
Akn <sibata486@gmail.com>
2024-10-21 23:17:51 +0900

    add feat / 指定ディレクトリのファイルとディレクトリのリストを取得するメソッドを追加。

--- PictureViewer/ViewModels/FileListViewModel.cs
+++ PictureViewer/ViewModels/FileListViewModel.cs
@@ -1,4 +1,6 @@
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using PictureViewer.Models;
 using Prism.Mvvm;
 
@@ -8,5 +10,16 @@ namespace PictureViewer.ViewModels
     public class FileListViewModel : BindableBase
     {
         public ObservableCollection<ExFileInfo> Files { get; set; } = new ();
+
+        private void LoadFileAndDirectories(string directoryPath)
+        {
+            var f = Directory.GetFiles(directoryPath)
+                .Select(p => new ExFileInfo(new FileInfo(p)));
+
+            var d = Directory.GetDirectories(directoryPath)
+                .Select(p => new ExFileInfo(new DirectoryInfo(p)));
+
+            Files = new ObservableCollection<ExFileInfo>(f.Concat(d).ToList());
+        }
     }
 }
