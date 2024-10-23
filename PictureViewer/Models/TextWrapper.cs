b0db9169b0eb9c8ddf761392cbd527afcc9de427
Akn <sibata486@gmail.com>
2024-10-23 16:27:47 +0900

    Update version number.

--- PictureViewer/Models/TextWrapper.cs
+++ PictureViewer/Models/TextWrapper.cs
@@ -29,7 +29,7 @@ namespace PictureViewer.Models
         [Conditional("RELEASE")]
         private void SetVersion()
         {
-            Version = "20241022" + "a";
+            Version = "20241023" + "a";
         }
 
         [Conditional("DEBUG")]
