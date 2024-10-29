6bccad8368f265224df3a31049200c0bc371701f
Akn <sibata486@gmail.com>
2024-10-29 22:16:52 +0900

    add / ListBox 上から、キー文字の入力でアイテムを選択する Behavior を実装。

---
+++ PictureViewer/Behaviors/KeySelectBehavior.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+using PictureViewer.ViewModels;
+
+namespace PictureViewer.Behaviors
+{
+    public class KeySelectBehavior : Behavior<ListBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.KeyDown += OnKeyDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.KeyDown -= OnKeyDown;
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender is not ListBox lb || lb.Items.Count == 0)
+            {
+                return;
+            }
+
+            if (e.Key is < Key.A or > Key.Z)
+            {
+                return;
+            }
+
+            if (lb.DataContext is not FileCopyDialogViewModel vm)
+            {
+                return;
+            }
+
+            vm.CopyFile(e.Key.ToString().ToLower().First());
+        }
+    }
+}
--- PictureViewer/ViewModels/FileCopyDialogViewModel.cs
+++ PictureViewer/ViewModels/FileCopyDialogViewModel.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using PictureViewer.Models;
 using PictureViewer.Views;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -9,14 +12,48 @@ namespace PictureViewer.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class FileCopyDialogViewModel : BindableBase, IDialogAware
     {
+        private ObservableCollection<ExFileInfo> copyableDirectories = new ();
+        private ObservableCollection<ExFileInfo> currentFiles = new ();
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => nameof(FileCopyDialog);
+
+        public ObservableCollection<ExFileInfo> CurrentFiles
+        {
+            get => currentFiles;
+            set => SetProperty(ref currentFiles, value);
+        }
+
+        public ObservableCollection<ExFileInfo> CopyableDirectories
+        {
+            get => copyableDirectories;
+            set => SetProperty(ref copyableDirectories, value);
+        }
 
         public DelegateCommand CloseCommand => new DelegateCommand(() =>
         {
             RequestClose?.Invoke(new DialogResult());
         });
+
+        /// <summary>
+        /// 入力された prefix をインデックスに変換し、CopyableDirectories の該当インデックスのディレクトリにファイルをコピーします。
+        /// </summary>
+        /// <param name="prefix">インデックスに変換するプレフィックスです。変換は a = 0, b = 1 .. という具合です。</param>
+        public void CopyFile(char prefix)
+        {
+            var targetIndex = prefix - 'a';
+            if (targetIndex < 0 || targetIndex >= copyableDirectories.Count)
+            {
+                return;
+            }
+
+            var dest = CopyableDirectories[targetIndex];
+            foreach (var exFileInfo in CurrentFiles)
+            {
+                File.Copy(exFileInfo.FileSystemInfo.FullName, dest.FileSystemInfo.FullName);
+            }
+        }
 
         public bool CanCloseDialog() => true;
 
