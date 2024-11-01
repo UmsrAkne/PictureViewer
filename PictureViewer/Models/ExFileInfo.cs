e9f9eb85e8537a84df1cec7b4b119aaba2440da4
Akn <sibata486@gmail.com>
2024-11-01 17:32:36 +0900

    add feat / 画像ファイルのパスからサムネイルを生成する処理を実装。

--- PictureViewer/Models/ExFileInfo.cs
+++ PictureViewer/Models/ExFileInfo.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Prism.Mvvm;
 
@@ -110,6 +111,50 @@ namespace PictureViewer.Models
                 Console.WriteLine(e.ToString());
                 return default;
             }
+        }
+
+        /// <summary>
+        /// 指定した画像ファイルを読み込み、指定された縦サイズのサムネイルを生成します。
+        /// </summary>
+        /// <param name="filePath">画像ファイルのパス</param>
+        /// <param name="desiredHeight">生成するサムネイルの縦サイズ（ピクセル単位）</param>
+        /// <returns>サムネイル画像の BitmapSource オブジェクト</returns>
+        private static BitmapSource GenerateThumbnail(string filePath, int desiredHeight)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("指定された画像ファイルが見つかりません。", filePath);
+            }
+
+            var originalImage = new BitmapImage();
+            originalImage.BeginInit();
+            originalImage.UriSource = new Uri(filePath);
+            originalImage.CacheOption = BitmapCacheOption.OnLoad; // ファイルを完全に読み込む
+            originalImage.EndInit();
+
+            // 元の画像の縦横比を保ちながら、縦サイズに基づいて横サイズを計算
+            var scale = (double)desiredHeight / originalImage.PixelHeight;
+
+            // サムネイル画像の生成
+            var thumbnail = new TransformedBitmap(originalImage, new ScaleTransform(scale, scale));
+
+            // SaveBitmapSourceToFile(thumbnail, outputPath);
+            return thumbnail;
+        }
+
+        /// <summary>
+        /// BitmapSource を指定のパスに PNG 形式で保存します。
+        /// </summary>
+        /// <param name="bitmapSource">保存する BitmapSource</param>
+        /// <param name="filePath">保存先のファイルパス</param>
+        private static void SaveBitmapSourceToFile(BitmapSource bitmapSource, string filePath)
+        {
+            var encoder = new PngBitmapEncoder(); // PNG エンコーダを使用
+            encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+
+            // 指定のパスにファイルを保存
+            using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            encoder.Save(fs);
         }
     }
 }
