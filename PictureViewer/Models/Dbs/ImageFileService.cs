2e05096e3e26b216992a143a8f827d0a9ac30906
Akn <sibata486@gmail.com>
2024-11-15 00:26:49 +0900

    add feat / ExFileInfo の取得 or 生成をするメソッドを ImageFileService に追加した。

--- PictureViewer/Models/Dbs/ImageFileService.cs
+++ PictureViewer/Models/Dbs/ImageFileService.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PictureViewer.Models.Dbs
@@ -20,6 +22,29 @@ namespace PictureViewer.Models.Dbs
         public Task AddImageFileAsync(ExFileInfo imageFile)
         {
             return imageFileRepository.AddAsync(imageFile);
+        }
+
+        /// <summary>
+        /// 引数に入力された文字列に基づき、ExFileInfo オブジェクトを取得します。
+        /// </summary>
+        /// <param name="imageFilePath">生成するファイルパス</param>
+        /// <returns>
+        /// 引数に渡された imageFilePath がデータベースに登録されているかを確認。<br/>
+        /// 登録済みならばデータベースから値を取り出し、未登録ならば新規生成したインスタンスをデータベースに登録したあとに返します。<br/>
+        /// </returns>
+        public async Task<ExFileInfo> GetOrCreateExFileAsync(string imageFilePath)
+        {
+            var exFileInfo = new ExFileInfo(new FileInfo(imageFilePath));
+            var all = await imageFileRepository.GetAllAsync();
+            var value = all.FirstOrDefault(f => f.FileSystemInfo.FullName == exFileInfo.FileSystemInfo.FullName);
+
+            if (value != null)
+            {
+                return value;
+            }
+
+            await imageFileRepository.AddAsync(exFileInfo);
+            return exFileInfo;
         }
     }
 }
