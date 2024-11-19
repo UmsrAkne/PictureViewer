636085ab479d4c36ce44fc0e3defdbe75a927974
Akn <sibata486@gmail.com>
2024-11-19 21:30:14 +0900

    refactoring / 規約に沿ってメソッドの記述順を変更した。

--- PictureViewer/Models/ExFileInfo.cs
+++ PictureViewer/Models/ExFileInfo.cs
@@ -94,12 +94,6 @@ namespace PictureViewer.Models
 
         private DirectoryInfo DirectoryInfo { get; set; }
 
-        public FileInfo GetThumbnailFileInfo()
-        {
-            var currentDirectory = new DirectoryInfo(ParentDirectoryPath);
-            return new FileInfo($@"Thumbnails\{currentDirectory.Name}\\{FileSystemInfo.Name}");
-        }
-
         /// <summary>
         /// 指定した画像ファイルを読み込み、指定された縦サイズのサムネイルを生成します。
         /// </summary>
@@ -142,6 +136,12 @@ namespace PictureViewer.Models
             // 指定のパスにファイルを保存
             using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
             encoder.Save(fs);
+        }
+
+        public FileInfo GetThumbnailFileInfo()
+        {
+            var currentDirectory = new DirectoryInfo(ParentDirectoryPath);
+            return new FileInfo($@"Thumbnails\{currentDirectory.Name}\\{FileSystemInfo.Name}");
         }
 
         /// <summary>
